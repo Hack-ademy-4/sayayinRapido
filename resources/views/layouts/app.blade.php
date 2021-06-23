791bc49b3f92f7477f45d42cc4498805031de893
nanci ramos <nanci-r@hotmail.com>
2021-06-23 13:00:04 +0200

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 
@@ -19,7 +20,7 @@
 
 
     <script src="{{mix('js/app.js')}}"></script>
-    <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
 
     <script>
         //script boton de logout
