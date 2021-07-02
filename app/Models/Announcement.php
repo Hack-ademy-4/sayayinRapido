d95e08c6420a05e26d383473951758e28d13c585
Benja <benjavalles@hotmail.com>
2021-07-02 11:26:04 +0200

    Model searchable

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -5,14 +5,28 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\AnnouncementImage;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Announcement extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = ["title", "body", "category_id", "price","is_accepted"];
+
+    public function toSearchableArray() {
+        $array = [
+            "id" => $this->id,
+            "title" => $this->title,
+            "body" => $this->body,
+            "category" => $this->category->name,
+            "other" => "announcement anuncio"
+        ];
+
+        return $array;
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
